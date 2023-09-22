be2f3ace38dbad5e31a77df93052d5c2c2c34a99
emma <eqbrown@isf.edu.hk>
2023-09-22 10:52:31 +0800

    update fo fall 23

--- circle_patterns.py
+++ circle_patterns.py
@@ -1,9 +1,8 @@
 from turtle import *
 
 def filled_circle(circle_size,circle_color):
-    pencolor(circle_color)
-    pensize(0)
     fillcolor(circle_color)
     begin_fill()
     circle(circle_size)
     end_fill()
+
